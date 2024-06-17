4ede6279cd35a59ca834dff9f95726059b5849d2
DenPaz <dppazlopez@gmail.com>
2024-06-17 02:04:32 -0300

    chore: Add script to create test users

---
+++ apps/users/management/commands/create_test_users.py
@@ -0,0 +1,61 @@
+from django.core.management.base import BaseCommand
+
+from ...models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        default_password = "2305"
+        test_super_users = [
+            {
+                "email": "dppazlopez@gmail.com",
+                "first_name": "Dennis",
+                "last_name": "Paz",
+                "is_superuser": True,
+            },
+            {
+                "email": "alissonpereira@gmail.com",
+                "first_name": "Alisson",
+                "last_name": "Pereira",
+                "is_superuser": True,
+            },
+        ]
+        test_staff_users = [
+            {
+                "email": "joaopereira@gmail.com",
+                "first_name": "João",
+                "last_name": "Pereira",
+                "is_staff": True,
+            },
+            {
+                "email": "crismetsen@gmail.com",
+                "first_name": "Cris",
+                "last_name": "Metsen",
+                "is_staff": True,
+            },
+        ]
+        test_users = [
+            {
+                "email": "xavieroliveira@gmail.com",
+                "first_name": "Xavier",
+                "last_name": "Oliveira",
+            },
+            {
+                "email": "andresilva@gmail.com",
+                "first_name": "Andre",
+                "last_name": "Silva",
+            },
+        ]
+        all_test_users = test_super_users + test_staff_users + test_users
+
+        for test_user in all_test_users:
+            obj, created = User.objects.get_or_create(
+                email=test_user["email"],
+                defaults={**test_user},
+            )
+            if created:
+                obj.set_password(default_password)
+                obj.save(update_fields=["password"])
+                self.stdout.write(self.style.SUCCESS(f"User {obj.email} created successfully!"))
+            else:
+                self.stdout.write(self.style.WARNING(f"User {obj.email} already exists!"))
---
+++ script.sh
@@ -0,0 +1,4 @@
+python manage.py makemigrations
+python manage.py migrate
+python manage.py create_test_users
+python manage.py runserver
